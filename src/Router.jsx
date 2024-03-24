03b241379f67bdcbc5b6e1aa0f7d05e10506af7b
Tushar Sahni <tusharsahni22@gmail.com>
2024-03-24 21:12:37 +0530

    Refactor Router component and add Layout component

--- src/Router.jsx
+++ src/Router.jsx
@@ -11,29 +11,40 @@ import ReturnandRefund from "./Component/Policy/returnandreturnpolicy"
 import Shipping from "./Component/Policy/shipping"
 import TermandCondition from "./Component/Policy/termsandservices"
 import Privacy from "./Component/Policy/Privacy";
-import Admin from './Component/Admin';
+// import Admin from './Component/Admin';
 import OrderSuccessPage from './Component/OrderSuccessPage';
 import ProcessingOrder from './Component/Checkout/PaymentProcesscing';
 import PrivateRoutes from './Component/PrivateRoute'
+
+function Layout({ children }) {
+  return (
+    <>
+      <Header/>
+      {children}
+    </>
+  );
+}
+
+
 function Router () {
   return (
     <BrowserRouter>
-    <Header/>
+    {/* <Header/> */}
       <Routes>
         <Route path='*' element={<h1>404 Not Found</h1>} />
-        <Route path='/' element={<Homepage />} />
-        <Route path='/products' element={<Product/>} />
-        <Route path='/product-description' element={<ProductDesc/>} />
-        <Route path='/policy/return&refund' element={<ReturnandRefund/>} />
-        <Route path='/policy/shipping-policy' element={<Shipping/>} />
-        <Route path='/policy/terms-of-service' element={<TermandCondition/>}/>
-        <Route path='/policy/privacy-policy' element={<Privacy/>}/>
-        <Route path='/admin' element={<Admin/>} />
+        <Route path='/' element={<Layout><Homepage/></Layout>} />
+        <Route path='/products' element={<Layout><Product/></Layout>} />
+        <Route path='/product-description' element={<Layout><ProductDesc/></Layout>} />
+        <Route path='/policy/return&refund' element={<Layout><ReturnandRefund/></Layout>} />
+        <Route path='/policy/shipping-policy' element={<Layout><Shipping/></Layout>} />
+        <Route path='/policy/terms-of-service' element={<Layout><TermandCondition/></Layout>}/>
+        <Route path='/policy/privacy-policy' element={<Layout><Privacy/></Layout>}/>
+        
 
         <Route path='/' element={<PrivateRoutes/>}>
-        <Route path='/myprofile' element={<MyProfile/>} />
-        <Route path='/checkout' element={<Checkout/>} />
-        <Route path='/order-success' element={<OrderSuccessPage/>} />
+        <Route path='/myprofile' element={<Layout><MyProfile/></Layout>} />
+        <Route path='/checkout' element={<Layout><Checkout/></Layout>} />
+        <Route path='/order-success' element={<Layout><OrderSuccessPage/></Layout>} />
         <Route path='/order/Processing-order/:id' element={<ProcessingOrder/>} />
         </Route>
       </Routes>
